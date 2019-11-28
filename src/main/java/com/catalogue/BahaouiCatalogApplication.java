db72a5732c036439e719cc7fc7b30fa535cdf167
Your Name <you@example.com>
2019-11-28 15:58:59 +0100

    first commit

---
+++ src/main/java/com/catalogue/BahaouiCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahaouiCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahaouiCatalogApplication.class, args);
+	}
+
+}
